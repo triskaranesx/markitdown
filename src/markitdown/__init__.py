bccb8b2b2786045546114d368dbecbd3af79a239
Triskara Nesx <triskaranesx@users.noreply.github.com>
2026-04-19 06:48:58 +0800

    feat: add package __init__ with public API exports

---
+++ src/markitdown/__init__.py
@@ -0,0 +1,6 @@
+"""MarkItDown - Convert various file formats to Markdown."""
+
+from markitdown._markitdown import MarkItDown, DocumentConverter, ConversionResult
+
+__version__ = "0.1.0"
+__all__ = ["MarkItDown", "DocumentConverter", "ConversionResult"]
